8d4316ea6c10e81ecb28d56be34f74dd0fdd7c70
abubakarwaheed12 <abubakarjutt6346527@gmail.com>
2023-08-01 17:46:44 +0500

    add

--- Celery-1/core/settings.py
+++ Celery-1/core/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-mqm)q^2zx98cvj3+o2wtyjte-jq8wv9fkw#9!)--o=g4fy284g
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'app',
 ]
 
 MIDDLEWARE = [
